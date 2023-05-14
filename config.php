ceb5910e2606ae7e4e6a2fa42ed9cafd00cd343a
Miguelgg11 <72270911+Miguelgg11@users.noreply.github.com>
2023-05-14 10:42:00 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Inicio.
+  </title>
+</head>
+
+<body>
+  <header>
+    <h1>Inicio</h1>
+    <p>
+        <a href="index1.php">Insertar Datos</a>
+
+
+
+        <a href="mostrar.php">Mostrar Datos</a>
+    </p>
+  </header>
+
+  <main>
+    <p>Eliga una opción</p>
+  </main>
+
+ </body>
+</html>
