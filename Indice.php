46197397cd07f28ac8cf24324c8d1fa98968dd5a
Miguelgg11 <72270911+Miguelgg11@users.noreply.github.com>
2023-05-21 17:49:23 +0200

    Add files via upload

---
+++ Indice.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Proyecto Azure</title>
+  <link rel="stylesheet" type="text/css" href="proyecto.css">
+</head>
+<body>
+  <h1>Proyecto Miguel Gomez</h1>
+  <ul>
+    <li><a href="Insertar.php">Inserta Datos</a></li>
+    <li><a href="mostrar.php">Mostrar Datos</a></li>
+  </ul>
+</body>
+</html>
---
+++ Insertar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Insertar</title>
+  <link rel="stylesheet" type="text/css" href="proyecto.css">
+</head>
+</head>
+<body>
+    <div>
+      <h1>Insertar un registro</h1>
+      <br>
+      <form method="POST" action="Insertar2.php">
+        Nombre
+        <br>
+        <input type="text" name="nombre"><br>
+        DNI
+        <br>
+        <input type="text" name="DNI"><br>
+        Telefono
+        <br> 
+        <input type="text" name="telefono">
+        <br>
+        <br>
+        <input type="submit" value="Insertar">
+        <input type="reset" value= "Reiniciar formulario">
+      </form>
+    </div>
+<p>
+    <a href="Indice.php">Volver al inicio</a>
+</p>
